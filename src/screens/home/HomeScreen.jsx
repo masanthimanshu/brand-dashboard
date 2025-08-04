1b7cc436e6646b4ecb2e02aec510db39d14ed8c2
Himanshu Masant <himanshu.masant@scoobies.ai>
2025-08-04 19:04:07 +0530

    updates

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -35,10 +35,56 @@ export default function HomeScreen() {
       </AppBar>
       <Drawer variant="persistent" anchor="left" open={open}>
         <Box width={300} p={2}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab pariatur
-          fuga odio, id quisquam deserunt molestiae? Explicabo consequuntur
-          dicta culpa dignissimos. Ut expedita sunt perferendis accusantium
-          natus dolor aut odit.
+          <img src="/logo.png" alt="Logo" width="100%" />
+          <br />
+          <br />
+          <Box
+            sx={{
+              borderRadius: 10,
+              cursor: "pointer",
+              fontWeight: "bold",
+              bgcolor: "lightgray",
+              padding: "0.5rem 1.5rem",
+            }}
+          >
+            <i class="fa-regular fa-house"></i> &nbsp; Home
+          </Box>
+          <br />
+          <Box
+            sx={{
+              borderRadius: 10,
+              cursor: "pointer",
+              fontWeight: "bold",
+              bgcolor: "lightgray",
+              padding: "0.5rem 1.5rem",
+            }}
+          >
+            <i class="fa-regular fa-house"></i> &nbsp; Home
+          </Box>
+          <br />
+          <Box
+            sx={{
+              borderRadius: 10,
+              cursor: "pointer",
+              fontWeight: "bold",
+              bgcolor: "lightgray",
+              padding: "0.5rem 1.5rem",
+            }}
+          >
+            <i class="fa-regular fa-house"></i> &nbsp; Home
+          </Box>
+          <br />
+          <Box
+            sx={{
+              borderRadius: 10,
+              cursor: "pointer",
+              fontWeight: "bold",
+              bgcolor: "lightgray",
+              padding: "0.5rem 1.5rem",
+            }}
+          >
+            <i class="fa-regular fa-house"></i> &nbsp; Home
+          </Box>
         </Box>
       </Drawer>
     </>
